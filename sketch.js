2465a46bf78014b9c301dbd842ed76f32b434d3c
Arnav <107131964+ArnavSinghDeopa@users.noreply.github.com>
2022-06-12 10:22:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,40 @@
-var trex, trex_running, edges;
-var groundImage;
 
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  groundImage = loadImage("ground2.png")
+function setup() {
+  createCanvas(1000,1000);
+  background(51);
+ 
+
 }
 
-function setup(){
-  createCanvas(600,200);
-  
-  // creating trex
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
-  edges = createEdgeSprites();
-  
-  //adding scale and position to trex
-  trex.scale = 0.5;
-  trex.x = 50
-}
+function draw() 
+{
 
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
 
-function draw(){
-  //set background color 
-  background("white");
-  
-  //logging the y position of the trex
-  console.log(trex.y)
-  
-  //jump when space key is pressed
-  if(keyDown("space")){
-    trex.velocityY = -10;
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
   }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+if(keyIsDown(RIGHT_ARROW)){
+  background("red");
+}
   
-  trex.velocityY = trex.velocityY + 0.5;
-  
-  //stop trex from falling down
-  trex.collide(edges[3])
   drawSprites();
 }
+
